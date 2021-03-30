60d5d3d0f7e3f35ed7ad0fa645e5defd3c14d40d
Sebasssiu <54748964+Sebasssiu@users.noreply.github.com>
2021-03-29 21:35:29 -0600

    Ready

--- src/views/exchangeRate.js
+++ src/views/exchangeRate.js
@@ -1,8 +1,74 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const ExchangeRate = () => {
+  const [data, setData] = useState({})
+  const [body, setBody] = useState({currency: 'USD'})
+  const today = () => {
+    var today = new Date()
+    var dd = today.getDate()
+    var mm = today.getMonth()+1
+    var yyyy = today.getFullYear()
+    if(dd<10){
+      dd='0'+dd
+    } 
+    if(mm<10){
+      mm='0'+mm
+    }
+    today = yyyy+'-'+mm+'-'+dd
+    return today
+  }
+
+  const getData = () => {
+    fetch('http://127.0.0.1:8000/exchangeRate/',{
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(body)
+    })
+    .then(response => response.json())
+    .then(response => setData(response))
+    .catch(error => setData(error))
+  }
+
   return (
-    <div>Exchange</div>
+    <>
+      <h1>Currency exchange rate</h1>
+      <label>Select currency:  </label>
+        <select 
+          onChange={(e) => setBody({
+            ...body,
+            currency: e.target.value
+          })} 
+          name="cars"
+          id="cars"
+        >
+        <option value="USD">USD</option>
+        <option value="EU">EU</option>
+      </select>
+      <br />
+      <label>Select Date:  </label>
+      <input 
+        type="date"
+        id="start"
+        name="trip-start"
+        min="2015-01-01"
+        max={today()}
+        onChange={(e) => setBody({
+          ...body,
+          date: e.target.value
+        })}
+      />
+      <br />
+      <button onClick={getData}>Request data</button>
+      {data?.coin ? (
+        <>
+          <h5>Date: {body.date}</h5>
+          <h5>Currency: {body.currency}</h5>
+          <h5>Rate: {data.exchangeRate}</h5>
+        </>
+      ) : null}
+    </>
   )
 }
 
--- src/views/exchangeRateComparison.js
+++ src/views/exchangeRateComparison.js
@@ -1,8 +1,90 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const ExchangeComparison = () => {
+  const [data, setData] = useState({})
+  const [body, setBody] = useState({currency: 'USD'})
+  const today = () => {
+    var today = new Date()
+    var dd = today.getDate()
+    var mm = today.getMonth()+1
+    var yyyy = today.getFullYear()
+    if(dd<10){
+      dd='0'+dd
+    } 
+    if(mm<10){
+      mm='0'+mm
+    }
+    today = yyyy+'-'+mm+'-'+dd
+    return today
+  }
+
+  const getData = () => {
+    fetch('http://127.0.0.1:8000/exchangeRateComparison/',{
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(body)
+    })
+    .then(response => response.json())
+    .then(response => setData(response))
+    .catch(error => setData(error))
+  }
+
   return (
-    <div>Comparison</div>
+    <>
+      <h1>Currency exchange rate</h1>
+      <label>Select currency:  </label>
+        <select 
+          onChange={(e) => setBody({
+            ...body,
+            currency: e.target.value
+          })} 
+          name="cars"
+          id="cars"
+        >
+        <option value="USD">USD</option>
+        <option value="EU">EU</option>
+      </select>
+      <br />
+      <label>Select start date:  </label>
+      <input 
+        type="date"
+        id="start"
+        name="trip-start"
+        min="2015-01-01"
+        max={today()}
+        onChange={(e) => setBody({
+          ...body,
+          date1: e.target.value
+        })}
+      />
+      <br />
+      <label>Select end date:  </label>
+      <input 
+        type="date"
+        id="start"
+        name="trip-start"
+        min="2015-01-01"
+        max={today()}
+        onChange={(e) => setBody({
+          ...body,
+          date2: e.target.value
+        })}
+      />
+      <br />
+      <button onClick={getData}>Request data</button>
+      {data?.mean ? (
+        <>
+          <h5>Start date: {body.date1}</h5>
+          <h5>End date: {body.date2}</h5>
+          <h5>Currency: {body.currency}</h5>
+          <h5>mean: {data.mean}</h5>
+          <h5>max: {data.max}</h5>
+          <h5>min: {data.min}</h5>
+        </>
+      ) : null}
+    </>
   )
 }
 
